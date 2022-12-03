254288c7e20e37a27f3ffa609f26c2ffca6bba1e
william.kibira <william.kibira@patientsky.com>
2022-12-03 07:51:27 +0300

    - move to using external yaml settings file - verify the existing path of file

--- app/core/configuration/base.go
+++ app/core/configuration/base.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/space-fold-technologies/aurora-agent/app/core/server"
 	"gopkg.in/yaml.v2"
@@ -24,5 +25,16 @@ func ParseFromResource() Configuration {
 		panic(err)
 	}
 	yaml.Unmarshal(yamlFile, &config)
+	return config
+}
+
+func ParseFromPath(filePath string) Configuration {
+	config := Configuration{}
+	if data, err := os.ReadFile(filePath); err != nil {
+		fmt.Println("Failed to open the file")
+		panic(err)
+	} else if err := yaml.Unmarshal(data, &config); err != nil {
+		panic(err)
+	}
 	return config
 }
--- main.go
+++ main.go
@@ -11,9 +11,10 @@ import (
 
 func main() {
 	mode := flag.String("mode", "run", " for the app run mode")
+	config := flag.String("config", "", "path to the configuration file")
 	flag.Parse()
-	if *mode == "run" || *mode == "RUN" {
-		application := app.Application{}
+	if *mode == "run" || *mode == "RUN" && len(*config) > 0 {
+		application := app.Application{Path: *config}
 		application.Start()
 		c := make(chan os.Signal, 2)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
